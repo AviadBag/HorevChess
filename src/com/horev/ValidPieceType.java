3f6c3580fbced8feb1196e5ebd8b241a2d4cead2
Aviad Bagno <aviad.bagno@gmail.com>
2021-12-10 13:52:16 +0200

    ValidPieceType.java: Simplified if statement.

--- src/com/horev/ValidPieceType.java
+++ src/com/horev/ValidPieceType.java
@@ -2,9 +2,7 @@ package com.horev;
 
 class ValidPieceType {
     public static boolean checkValidPieceType(String piece) {
-        if (piece.equals("rook") || piece.equals("knight") || piece.equals("bishop") || piece.equals("queen") || piece.equals("king")
-                || piece.equals("pawn"))
-            return true;
-        return false;
+        return piece.equals("rook") || piece.equals("knight") || piece.equals("bishop") || piece.equals("queen") || piece.equals("king")
+                || piece.equals("pawn");
     }
 }
